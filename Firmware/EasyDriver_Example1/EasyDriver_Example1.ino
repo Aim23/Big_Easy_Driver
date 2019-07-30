618808060e8a2b2228acb335f25e7690638cdded
Aim23 <Aim23@users.noreply.github.com>
2019-07-30 18:57:58 +0200

    Update EasyDriver_Example1.ino
    Example URL has changed.

--- Firmware/EasyDriver_Example1/EasyDriver_Example1.ino
+++ Firmware/EasyDriver_Example1/EasyDriver_Example1.ino
@@ -1,5 +1,5 @@
 // Example1 code for Brian Schmalz's Easy Driver Example page
-// http://www.schmalzhaus.com/EasyDriver/EasyDriverExamples.html
+// http://www.schmalzhaus.com/EasyDriver/Examples/EasyDriverExamples.html
 
 void setup() {                
   pinMode(8, OUTPUT);     
